f9d5a374d3bc0fb6390aac5bcccdd50f60f0419d
RaisanJMR <raisanjmr@protonmail.com>
2021-07-22 22:19:51 +0530

    working on selector hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import BookContainer from './Components/bookContainer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import HooksCakecontainer from './Components/hooksCakecontainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-       <BookContainer/>
+        <BookContainer />
+        <HooksCakecontainer />
       </div>
     </Provider>
   );
